86d31acf0b1a67acf93e5f02418603f424c45292
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-09 13:11:28 +0300

    basic author template

---
+++ src/books/author.ts
@@ -0,0 +1,13 @@
+export class Author {
+    id: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(id: number, firstName: string, lastName: string) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+export type AuthorFields = [number, string, string];
