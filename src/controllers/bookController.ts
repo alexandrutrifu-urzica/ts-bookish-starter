2060c34703a11fcf9d77f2e55fe811e7c3fb6979
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-09 10:41:49 +0300

    refactored methods

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -2,31 +2,50 @@ import { Router, Request, Response } from 'express';
 import { Request as TediousRequest } from 'tedious';
 
 import { connection } from '../app';
-import { Book } from '../book';
+import { getCatalogue, getBookByID } from '../books/bookEndpointMethods';
+import { BookQuerier } from '../books/queries';
 
 class BookController {
     router: Router;
+    querier: BookQuerier;
 
     constructor() {
         this.router = Router();
+        this.router.get('/id/:bookID', this.getBookByID.bind(this));
         this.router.get('/catalogue', this.getLibraryCatalogue.bind(this));
 
         this.router.post('/', this.createBook.bind(this));
+
+        // Instantiate querier
+        this.querier = new BookQuerier();
+    }
+
+    async getBookByID(req: Request, res: Response) {
+        const bookID = req.params.bookID;
+        const query = this.querier.getBookByIDQuery(bookID);
+
+        const request = new TediousRequest(query, (err, rowCount: number) => {
+            if (err) {
+                res.send(`Error: ${err}`);
+            } else {
+                console.log(rowCount);
+            }
+        });
+
+        res.send(await getBookByID(connection, request));
     }
 
     async getLibraryCatalogue(req: Request, res: Response) {
-        const request = new TediousRequest(
-            'select * from bookish.dbo.BOOKS',
-            (err, rowCount: number) => {
-                if (err) {
-                    console.log('Error:', err);
-                } else {
-                    console.log(rowCount);
-                }
-            },
-        );
+        const query = this.querier.getCatalogueQuery();
+        const request = new TediousRequest(query, (err, rowCount: number) => {
+            if (err) {
+                res.send(`Error: ${err}`);
+            } else {
+                console.log(rowCount);
+            }
+        });
 
-        res.send(await Book.getBooks(connection, request));
+        res.send(await getCatalogue(connection, request));
     }
 
     createBook(req: Request, res: Response) {
