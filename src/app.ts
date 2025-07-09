c6ee59b0037cc9930fa82e8b2f8cb351939510dc
Alexandru Trifu-Urzica <alexandru.trifu-urzica@softwire.com>
2025-07-09 10:15:13 +0300

    configured express and tedious

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,44 @@
 import express from 'express';
 import 'dotenv/config';
+import { Connection, ConnectionConfiguration } from 'tedious';
 
 import healthcheckRoutes from './controllers/healthcheckController';
 import bookRoutes from './controllers/bookController';
 
-const port = process.env['PORT'] || 3000;
+const port = process.env['PORT'] || 31289;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+/**
+ * Tedious config
+ */
+const config: ConnectionConfiguration = {
+    server: 'localhost', // or "localhost"
+    options: {
+        trustServerCertificate: true,
+    },
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'test',
+            password: 'Parola123#',
+        },
+    },
+};
+
+export const connection = new Connection(config);
+
+connection.on('connect', function (err) {
+    if (err) {
+        console.log('Error: ', err);
+    }
+});
+
+connection.connect();
 
 /**
  * Primary app routes.
